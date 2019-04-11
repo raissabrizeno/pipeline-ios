6c35bc16870f1599a016d4fb938755e28ea99e3f
raissabrizeno <raissa.brizeno@gmail.com>
2019-04-11 13:48:28 -0300

    projeto

---
+++ fastlane101Tests/fastlane101Tests.swift
@@ -0,0 +1,33 @@
+//
+//  fastlane101Tests.swift
+//  fastlane101Tests
+//
+//  Created by alessandra.l.pereira on 04/04/19.
+//  Copyright © 2019 alessandra.l.pereira. All rights reserved.
+//
+
+import XCTest
+
+class fastlane101Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
